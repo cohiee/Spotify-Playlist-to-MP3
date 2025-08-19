ca59dabe66c05cc075057df5f1e11412634ec7c5
Vincent C <108943454+cohiee@users.noreply.github.com>
2025-08-20 04:02:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+_SPOTIPY_CLIENT_ID = "c8ee484ca8f4479faeed26689dce086f"
+_SPOTIPY_CLIENT_SECRET = "84f76f7170984df8b4dd7b58bb45ea92"
+_SPOTIPY_REDIRECT_URI = "http://127.0.0.1:5000/"
+
+def get_client_id():
+    return _SPOTIPY_CLIENT_ID
+
+def get_client_secret():
+    return _SPOTIPY_CLIENT_SECRET
+
+def get_redirect_uri():
+    return _SPOTIPY_REDIRECT_URI
